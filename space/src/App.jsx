91d2699ef6eb27ff3d1209b08755e74eb797ef5e
Neo <lordneobarnachea@gmail.com>
2023-05-08 16:51:56 +0800

    added react router

--- space/src/App.jsx
+++ space/src/App.jsx
@@ -1,13 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-
+import Crew from './components/Crew'
+import Destination from './components/Destination'
+import Technology from './components/Technology'
 function App() {
 
-  return (
-    <>
-     <Home/>
-     
-    </>
-  )
-}
+  return(
+  <Home/> 
+  ) 
+  }
 
 export default App
--- space/src/components/Home.jsx
+++ space/src/components/Home.jsx
@@ -1,19 +1,24 @@
+import { Link, Route, Routes } from 'react-router-dom'
+
+import Crew from './Crew'
+import Destination from './Destination'
+import Technology from './Technology'
 import React from 'react'
 import styles from '../styles/home.module.css'
 export default function Home() {
     return (
         <>
-            <div className={styles.body}>
+            <div className={styles.container}>
                 <div className={styles.navigation}>
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF" /><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z" /></g></svg>
                     </div>
                     <div className={styles.line}></div>
                     <div className={styles.tags}>
-                    <a href="">00 Home</a>
-                       <a href="">01 Destination</a>
-                       <a href=""> 02 Crew</a> 
-                       <a href="">03 Technology</a> 
+                        <Link to='/'><span>00</span> Home</Link>
+                        <Link to='Destination'> <span>01</span> Destination</Link>
+                        <Link to='Crew'><span>02</span> Crew</Link> 
+                        <Link to='Technology'><span>03</span> Tecnology</Link> 
                     </div>
                 </div>
 
@@ -28,8 +33,15 @@ export default function Home() {
                 <div className={styles.circle}>
                     Explore
                 </div>
-
+                
             </div>
+            
+            <Routes>
+               
+                <Route path='/Destination'element={<Destination/>} />
+                <Route path='/Crew' element={<Crew/>}/>
+                <Route path='/Technology' element={<Technology/>}/>
+            </Routes>
         </>
 
     )
