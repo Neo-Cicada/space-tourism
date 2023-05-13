98b3ce8d3f2d9ba95e10d5b1211a6eb07d4a745f
Neo <lordneobarnachea@gmail.com>
2023-05-13 10:52:31 +0800

    added div boxes in technology components

--- space/src/components/Technology.jsx
+++ space/src/components/Technology.jsx
@@ -3,7 +3,22 @@ import '../styles/technology.css'
 export default function Technology() {
   return (
     <div className="container">
+      <div className="launch">
+
+      </div>
+
+      <div className='techBox'>
+
+      </div>
       
+      <div className="techCircles" id='cOne'></div>
+      <div className="techCircles" id='cTwo'></div>
+      <div className="techCircles" id='cThree'></div>
+
+      
+      <div className="techImg">
+
+      </div>
     </div>
   )
 }
