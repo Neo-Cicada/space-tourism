eb465bd3ddcedfd57c79d6e4c4121343c4603514
Neo <lordneobarnachea@gmail.com>
2023-05-17 21:10:18 +0800

    added hover effect on the circle home

--- space/src/components/Home.jsx
+++ space/src/components/Home.jsx
@@ -17,9 +17,14 @@ export default function Home(props) {
                         outer space and not hover kind of on the edge of it. Well sit back, and relax
                         because we'll give you a truly out of this world experience!</p>
                 </div>
-                <div className={styles.circle}>
-                    Explore
-                </div>
+
+                
+                    <div className={styles.circle}>
+                        Explore
+                    </div>
+                
+
+                
              
                 
 
